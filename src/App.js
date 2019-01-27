0a4c4db4986859f87327c4291a60e115db3f5e42
Igor Knop <igorknop@gmail.com>
2019-01-27 13:29:01 -0200

    Add style to cards

--- src/App.js
+++ src/App.js
@@ -7,42 +7,70 @@ const CARDS = [
   {
     left: { type: "Quarto", w: 3, h: 5 },
     right: { type: "Adulto", w: 1, h: 2 }
+  },
+  {
+    left: { type: "Banheiro", w: 2, h: 2 },
+    right: { type: "Quarto", w: 5, h: 3 }
   }
 ];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { n: 0 };
+  }
   render() {
-    let card = CARDS[0];
+    let allCardsE = CARDS.map((card, k) => {
+      return (
+        <main className="card" key={"card" + k}>
+          <table>
+            <tbody>
+              <tr id="left">
+                <td>
+                  <div
+                    className={"symbol " + card.left.type}
+                    style={{
+                      width: card.left.w * S + "mm",
+                      height: card.left.h * S + "mm"
+                    }}
+                  />
+                </td>
+                <td>
+                  <div
+                    className={"symbol " + card.right.type}
+                    style={{
+                      width: card.right.w * S + "mm",
+                      height: card.right.h * S + "mm"
+                    }}
+                  />
+                </td>
+              </tr>
+              <tr id="right">
+                <td className="name">
+                  {card.left.type} {card.left.w}x{card.left.h}
+                </td>
+                <td className="name">
+                  {card.right.type} {card.right.w}x{card.right.h}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </main>
+      );
+    });
 
     return (
       <div className="App">
-        <main id="card">
-          <table>
-            <tr id="left">
-              <td>
-                <div
-                  className={"symbol " + card.left.type}
-                  style={{ width: card.left.w * S+"mm", height: card.left.h * S+"mm" }}
-                >
-                </div>
-              </td>
-              <td>
-                <div
-                  className={"symbol " + card.right.type}
-                  style={{ width: card.right.w * S+"mm", height: card.right.h * S+"mm" }}
-                />
-              </td>
-            </tr>
-            <tr id="right">
-              <td className="name">
-                {card.left.type} {card.left.w}x{card.left.h}
-              </td>
-              <td className="name">
-                {card.right.type} {card.right.w}x{card.right.h}
-              </td>
-            </tr>
-          </table>
-        </main>
+        <div
+          onClick={() => {
+            let k = this.state.n + 1;
+            if (k >= allCardsE.length) k = 0;
+            this.setState({ n: k });
+          }}
+        >
+          {allCardsE[this.state.n]}
+        </div>
+        <div id="allCards">{allCardsE}</div>
       </div>
     );
   }
