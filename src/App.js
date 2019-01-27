34280cb35ece9dd709e5b0affcc6afd87b73be3e
Igor Knop <igorknop@gmail.com>
2019-01-27 13:06:01 -0200

    Add basic card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const S = 5;
+const CARDS = [
+  {
+    left: { type: "Quarto", w: 3, h: 5 },
+    right: { type: "Adulto", w: 1, h: 2 }
+  }
+];
 
 class App extends Component {
   render() {
+    let card = CARDS[0];
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <main id="card">
+          <table>
+            <tr id="left">
+              <td>
+                <div
+                  className={"symbol " + card.left.type}
+                  style={{ width: card.left.w * S+"mm", height: card.left.h * S+"mm" }}
+                >
+                </div>
+              </td>
+              <td>
+                <div
+                  className={"symbol " + card.right.type}
+                  style={{ width: card.right.w * S+"mm", height: card.right.h * S+"mm" }}
+                />
+              </td>
+            </tr>
+            <tr id="right">
+              <td className="name">
+                {card.left.type} {card.left.w}x{card.left.h}
+              </td>
+              <td className="name">
+                {card.right.type} {card.right.w}x{card.right.h}
+              </td>
+            </tr>
+          </table>
+        </main>
       </div>
     );
   }
