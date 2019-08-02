7c7a1d2910d2ad787c46eeee4db7bcb644880546
Igor Knop <igorknop@gmail.com>
2019-08-02 20:36:19 -0300

    Add files via upload

---
+++ precache-manifest.d764992db809cd86ff3e214f0f8c7f4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "370d7ae2b92640e1ec7d911645b3dd13",
+    "url": "/puxadinho/index.html"
+  },
+  {
+    "revision": "46d0e79244aebaf892e9",
+    "url": "/puxadinho/static/css/main.3809fa5a.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/puxadinho/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "46d0e79244aebaf892e9",
+    "url": "/puxadinho/static/js/main.2243e410.chunk.js"
+  },
+  {
+    "revision": "44c729cc78c8f424d74e",
+    "url": "/puxadinho/static/js/runtime~main.13410b47.js"
+  },
+  {
+    "revision": "08b218e9443f85244a2736beab9fc487",
+    "url": "/puxadinho/static/media/fundo.08b218e9.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/puxadinho/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/puxadinho/precache-manifest.a95118c13a05b747af1e75a3d094cb3e.js"
+  "/puxadinho/precache-manifest.d764992db809cd86ff3e214f0f8c7f4a.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/puxadinho/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/puxadinho/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
